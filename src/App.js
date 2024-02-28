ee83227641aafa0e053ab735c2ad2480a3996ebc
judyl2 <149786355+judyl2@users.noreply.github.com>
2024-02-28 17:57:04 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header"></header>
+			<div className="container">
+				<h1>Weather App</h1>
+
+				{/* <button className="btn btn-warning">Search</button> */}
+				<footer>
+					This was coded by <a href="#">Judy</a>{" "}
+				</footer>
+			</div>
 		</div>
 	);
 }
